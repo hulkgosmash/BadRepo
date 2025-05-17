9e07e73ed1924311b890d7d1da2cdd974e7bac1d
HulkGoSmash <115122204+hulkgosmash@users.noreply.github.com>
2025-05-17 14:54:53 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,21 +2,12 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_security_group" "bad_sg" {
-  name        = "allow_all"
-  description = "Allows all traffic"
-  
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_s3_bucket" "public_bucket" {
+  bucket = "my-insecure-bucket"
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  acl    = "public-read"
+
+  versioning {
+    enabled = false
   }
 }
